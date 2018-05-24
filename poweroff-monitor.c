abed59fefa7205f9d0cc9c7df662a5f3efd475d6
Nick Bulleid <nedbulleid@fastmail.com>
2018-05-24 19:56:02 +0000

    Tidyup and comment

--- poweroff-monitor.c
+++ poweroff-monitor.c
@@ -1,28 +1,39 @@
 #include <fcntl.h>
 #include <poll.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 void poll_pin() {
     struct pollfd fdlist[1];
     int fd;
+    char buf[3];
 
-    fd = open("/sys/class/gpio/gpio26/value", O_RDONLY);
+    buf[2] = '\0';
+
+    // TODO - set the edge of the gpio to "both"
+
+    fd = open("/sys/class/gpio/gpio6/value", O_RDONLY);
+    read(fd, buf, 2);
     fdlist[0].fd = fd;
     fdlist[0].events = POLLPRI;
 
     while (1) {
         int err;
-        char buf[3];
 
         err = poll(fdlist, 1, -1);
         if (err == -1) {
             perror("poll");
-            return;
+            exit(1);
         }
 
-        err = read(fdlist[0].fd, buf, 2);
-        printf("event on poweroff pin\n");
+        if (fdlist[0].revents & POLLPRI) {
+            printf("event on poweroff pin\n");
+            lseek(fdlist[0].fd, 0, SEEK_SET);
+            read(fdlist[0].fd, buf, 2);
+
+            // TODO - debounce input and call shutdown
+        }
     }
 }
 
